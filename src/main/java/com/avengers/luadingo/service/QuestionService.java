fe9559eba910eb8f4238dc50c2393f428c729693
Bequery10 <Berkan.gokgoz@tedu.edu.tr>
2024-05-17 11:07:49 +0300

    Update QuestionService.java

--- src/main/java/com/avengers/luadingo/service/QuestionService.java
+++ src/main/java/com/avengers/luadingo/service/QuestionService.java
@@ -3,15 +3,17 @@ package com.avengers.luadingo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import java.util.*;
 
 import com.avengers.luadingo.model.Question;
 
 public interface QuestionService {
-    
-    public Question save(Question quiz);
+
+    public int save(Question quiz);
+
     public List<Question> getAll();
+
     public Question get(int question_id);
-    public void delete(int question_id);
+
+    public int delete(int question_id);
 }
