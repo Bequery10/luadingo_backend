5c5adc97f2e4e61ffcf47449b0e93e66eae66165
Bequery10 <Berkan.gokgoz@tedu.edu.tr>
2024-05-19 20:47:10 +0300

    Update AttemptsRepository.java

--- src/main/java/com/avengers/luadingo/repository/AttemptsRepository.java
+++ src/main/java/com/avengers/luadingo/repository/AttemptsRepository.java
@@ -56,7 +56,7 @@ public class AttemptsRepository {
             return 1;
 
         String sql = "SELECT COUNT(*) " +
-                "FROM (SELECT quiz_id FROM course JOIN attempts ON course.quiz_id = attempts.quiz_id WHERE course_id = ?) AS subquery";
+                "FROM (SELECT attempts.quiz_id FROM has_quiz JOIN attempts ON has_quiz.quiz_id = attempts.quiz_id WHERE has_quiz.course_id = ?) AS subquery";
         int count = jdbcTemplate.queryForObject(sql, Integer.class, course_id);
         if (count == 2) {
             String sql1 = "INSERT INTO badge (column_name) " +
