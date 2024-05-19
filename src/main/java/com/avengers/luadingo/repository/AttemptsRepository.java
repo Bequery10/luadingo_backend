3c63984565f266f4340a6d0c7e16140790fb5562
Bequery10 <Berkan.gokgoz@tedu.edu.tr>
2024-05-19 23:08:34 +0300

    Update AttemptsRepository.java

--- src/main/java/com/avengers/luadingo/repository/AttemptsRepository.java
+++ src/main/java/com/avengers/luadingo/repository/AttemptsRepository.java
@@ -47,34 +47,24 @@ public class AttemptsRepository {
         return jdbcTemplate.update(sql, username, attempt_id, quiz_id);
     }
 
-    public int earnBadge(String username, int course_id, int attempt_id) {
+    public int earnBadge(String username, int quiz_id, int attempt_id) {
 
         String sql2 = "SELECT attempt_current from attempt where attempt_id = ?";
         int current = jdbcTemplate.queryForObject(sql2, Integer.class, attempt_id);
 
         if (current != 5)
             return 1;
-
-        String sql = "SELECT COUNT(*) " +
-                "FROM (SELECT attempts.quiz_id FROM has_quiz JOIN attempts ON has_quiz.quiz_id = attempts.quiz_id WHERE has_quiz.course_id = ?) AS subquery";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class, course_id);
+        System.out.println("-----------------------------------------------------0----------------------------");
+        String sql = "SELECT COUNT(*) FROM attempts atts JOIN attempt a ON atts.attempt_id = a.attempt_id JOIN has_quiz hq ON atts.quiz_id = hq.quiz_id WHERE atts.username = ? AND hq.course_id = (SELECT course_id FROM has_quiz WHERE quiz_id = ?) AND a.attempt_score = 500;";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, username, quiz_id);
         if (count == 2) {
-            String sql1 = "INSERT INTO badge (column_name) " +
-                    "SELECT badge_id " +
-                    "FROM badge " +
-                    "JOIN course ON badge.desc = course.language " +
-                    "WHERE course.course_id = ? " +
-                    "LIMIT 1";
-            jdbcTemplate.update(sql1, username, course_id);
+            String sql1 = "INSERT INTO has_badge (username, badge_id) SELECT ?, badge.badge_id FROM badge JOIN course ON badge.badge_desc = course.course_language JOIN has_quiz ON course.course_id = has_quiz.course_id WHERE has_quiz.quiz_id = ? LIMIT 1;";
+            jdbcTemplate.update(sql1, username, quiz_id);
             return 2;
         } else if (count == 5) {
-            String sql1 = "INSERT INTO badge (column_name) " +
-                    "SELECT badge_id " +
-                    "FROM badge " +
-                    "JOIN course ON badge.desc = course.language " +
-                    "WHERE course.course_id = ? " +
-                    "LIMIT 1,1";
-            jdbcTemplate.update(sql1, username, course_id);
+            String sql1 = "INSERT INTO has_badge (username, badge_id) SELECT ?, badge.badge_id FROM badge JOIN course ON badge.badge_desc = course.course_language JOIN has_quiz ON course.course_id = has_quiz.course_id WHERE has_quiz.quiz_id = ? LIMIT 1,1;";
+            jdbcTemplate.update(sql1, username, quiz_id);
+
             return 3;
         }
 
